630b61572df3c2373ca67211543a2ae60c89e7e4
Mauricio Pulgarin <mauriciopul@gmail.com>
2019-03-10 17:46:09 -0500

    conexion a base de datos creada

--- src/app.js
+++ src/app.js
@@ -1,6 +1,38 @@
+const path = require ('path');
 const express = require ('express');
+const morgan = require ('morgan');
+const mongoose = require ('mongoose');
+
 const app = express();
 
-app.listen(4000, ()=>{
-    console.log('Escuchando por el puerto 4000');
+//Conectando a la base de datos
+mongoose.connect('mongodb://localhost/crud-mongo')
+    .then(db => console.log('Base de datos conectada'))
+    .catch(err => console.log('Error al conectar con la base de datos'));
+
+
+
+//importar rutas
+const indexRoutes = require('./routes/index');
+
+
+//configuraciones
+//Toma el puerto que asigna el S.O. si no esta libre entonces asigna el puerto 3000
+app.set('port', process.env.PORT || 3000); 
+app.set('views', path.join(__dirname , 'views'))//el comando __dirname indica la ubicación actual de este archivo
+app.set ('view engine', 'ejs')
+
+
+
+//middelwares (una funcion que se ejecuta antes de llegar a las rutas)
+app.use(morgan('dev'));
+
+
+//rutas (routes)
+app.use ('/', indexRoutes);
+app.use (express.urlencoded({extended:false}));
+
+//iniciar el servidor
+app.listen(app.get('port'), ()=>{
+    console.log(`Servidor en el puerto ${app.get('port')}`);
 })
