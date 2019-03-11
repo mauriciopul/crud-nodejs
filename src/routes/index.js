d25d8f42eebc63f58e1ceb26b10dc0da1a8c52c8
Mauricio Pulgarin <mauriciopul@gmail.com>
2019-03-10 21:51:45 -0500

    El CRUD ya elimina y muestra 'hecho'

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,15 +5,43 @@ const router = express.Router();
 const Tarea = require('../models/taks');
 
 
-router.get('/', (req, res) => {
-    res.render('index')
+router.get('/', async(req, res) => {
+    const tareas =  await Tarea.find();
+    res.render('index', {
+        tareas
+    });
 });
 
 router.post('/add', async (req, res) => {
     const tarea = new Tarea(req.body);
     await tarea.save();
-    res.send('Recibido');
+    res.redirect('/');
 });
+
+router.get('/convertir/:id', async (req, res) => {
+    const { id } = req.params;
+    const tarea = await Tarea.findById(id);
+    tarea.estado = !tarea.estado;
+    await tarea.save();
+    res.redirect('/');
+});
+
+router.get('/editar/:id', async (req,res) =>{
+    const { id } = req.params;
+    const tarea = await Tarea.findById(id);
+    res.render('edit')
+    
+})
+
+
+
+router.get('/delete/:id', async (req, res)=>{
+    const { id } = req.params;
+    await Tarea.remove({_id: id});
+    res.redirect('/');
+});
+
+
 
 module.exports = router;
 
